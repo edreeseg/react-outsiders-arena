33ab6e628d8baff43e0a65327d6e3b4db2381879
edreeseg <edward.reeseg@gmail.com>
2018-12-09 11:41:53 -0500

    More work on display bar

--- src/components/DisplayBar/DisplayBar.js
+++ src/components/DisplayBar/DisplayBar.js
@@ -9,6 +9,7 @@ class DisplayBar extends React.Component {
       nameInput: '',
       avatarInput: '',
       opponentInput: '',
+      authed: this.props.auth,
     };
   }
   handleChangeFor = (type) => (e) => {
@@ -25,8 +26,20 @@ class DisplayBar extends React.Component {
     };
     fetch(`http://${URL.roren}/api/player/`, {
       method: 'POST',
-      body: JSON.stringify({ displayName: this.state.nameInput, avatarUrl: this.state.avatarInput }),
-    }).then(res => this.setState({ playerID: res.id, nameInput: '', avatarInput: '', }))
+      body: JSON.stringify({ 
+        displayName: this.state.nameInput || 'NPC', 
+        avatarUrl: this.state.avatarInput || 'https://i.imgur.com/sdOs51i.jpg'
+      })
+    }).then(res => this.setState((prevState) => {
+      return {
+        nameInput: '',
+        avatarInput: '',
+        playerID: res.id,
+        playerName: prevState.nameInput,
+        avatar: prevState.avatarInput,
+        authed: true,
+      }
+    }))
       .catch(err => { throw new Error(err) });
   }
   render(){
@@ -34,15 +47,22 @@ class DisplayBar extends React.Component {
       <section className="display-bar">
         <div className="session-info">
           <h3>Arena ID:</h3>
-          <p>{this.props.arenaID || 'Unknown'}</p>
+          <p>{this.state.arenaID || 'Unknown'}</p>
           <h3>Player ID:</h3>
-          <p>{this.props.playerID || 'Unknown'}</p>
+          <p>{this.state.playerID || 'Unknown'}</p>
         </div>
-        <form className="create-player">
-          <input type="text" placeholder="Display Name" value={this.state.nameInput} onChange={this.handleChangeFor('name')} />
-          <input type="text" placeholder="Avatar URL" value={this.state.avatarInput} onChange={this.handleChangeFor('avatar')} />
-          <button type="button" onClick={this.sendConnectRequest}>Submit</button>
-        </form>
+        {this.state.authed ? (
+          <div className="player-info">
+            <img src={this.state.avatar} alt="User avatar" />
+            <h2>{this.state.playerName}</h2>
+          </div>
+        ) : (
+          <form className="create-player">
+            <input type="text" placeholder="Display Name" value={this.state.nameInput} onChange={this.handleChangeFor('name')} />
+            <input type="text" placeholder="Avatar URL" value={this.state.avatarInput} onChange={this.handleChangeFor('avatar')} />
+            <button type="button" onClick={this.sendConnectRequest}>Submit</button>
+          </form>
+        )}
         <form className="find-battle">
           <input type="text" placeholder="Opponent's Name" value={this.state.opponentInput} onChange={this.handleChangeFor('opponent')} />
           <button type="submit">Find Battle</button>
